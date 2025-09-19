a46dea35c3a6483a076a0e44816f9571fd0242ec
Stephan Mendes <stephan.mendesnew@gmail.com>
2025-09-19 18:11:46 -0300

    [FEAT]: Tela Cestas(app)

---
+++ Frontend/cestas_app/lib/pages/basket/basket_page.dart
@@ -0,0 +1,65 @@
+import 'package:cestas_app/main.dart';
+import 'package:flutter/material.dart';
+import 'package:cestas_app/widgets/app_drawer.dart';
+import 'package:core/widgets/statCard.dart';
+
+
+
+class BasketPage extends StatefulWidget {
+  const BasketPage({super.key});
+
+  @override
+  _BasketPageState createState() => _BasketPageState();
+}
+
+class _BasketPageState extends State<BasketPage>{
+//Definição dos estados para os checkbox das familias
+
+  bool isSelectedMaria = false;
+  bool isSelectedAna = false;
+  bool isSelectedJoao = false;
+
+
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Gestão de Cestas'),
+        backgroundColor: Colors.blueAccent,
+      ),
+      drawer: const AppDrawer(),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TypeBasket(),
+            const SizedBox(height: 24),
+            FamilyList(
+              isSelectedMaria: isSelectedMaria,
+              isSelectedAna: isSelectedAna,
+              isSelectedJoao: isSelectedJoao,
+              onChangedMaria: (value){
+                setState(() {
+                  isSelectedMaria = value ?? false;
+                });
+              },
+              onChangedAna: (value){
+                  setState(() {
+                    isSelectedAna = value ?? false;
+                  });
+              },
+              onChangeJoao: (value){
+                setState(() {
+                  isSelectedJoao = value ?? false;
+                });
+              },
+            ),
+            const SizedBox(height: 24),
+            GenerateBasket(),
+          ],
+        ),
+      ),
+    );
+  }
+}
