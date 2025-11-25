3ea69aaf565ac2223b1f211170c9123c57e4842b
Kalliel Pinheiro <kallielmpinheiro2004@gmail.com>
2025-11-24 23:07:17 -0300

    add: testes

--- Backend/app/models/base_modal.py
+++ Backend/app/models/base_modal.py
@@ -1,4 +1,4 @@
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy import Boolean
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.sql import func
